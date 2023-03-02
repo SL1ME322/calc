9e3f802b0cab57a09266d1c2587a865759f6e3fe
SL1ME322 <102655919+SL1ME322@users.noreply.github.com>
2023-03-02 16:48:40 +0300

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,128 @@
-func = int(input("Выберите функцию:\n1)Сложение\n2)Вычитание\n3)Умножение\n4)Деление\n"))
-count = int(input("Введите кол-во чисел:"))
-i=1
-result = int(input("Введите первое число"))
-for i in range(count):
-    a = int(input("Введите число: ")) 
-    if func == 1:
-       result = result + a
-    if func == 2:
-       result = result - a
-    if func == 3:
-       result = result * a
-    if func == 4:
-       result = result / a
-    print(result)
+result = 0
+year = int(input('Введите год: '))
+mult = 1
+#Январь
+for i in range (1, 32):
+    if (i>9):
+            d1 = i % 10
+            d2 = i % 100 // 10
+            result+= d1 + d2
+           
+    if(i<10):
+           
+             result+=i
+ 
+#Февраль
+if(year%4 ==0 and year%100!=0 or year%400 == 0):
+     
+    for i in range (1, 30):
+         
+        if (i>9):
+            d1 = i % 10
+            d2 = i % 100 // 10
+            result+= d1 + d2
+        if(i<10):
+             result+=i
+else:
+    for i in range (1, 29):
+         if (i>9):
+            d1 = i % 10
+            d2 = i % 100 // 10
+            result+= d1 + d2
+         if(i<10):
+            result+=i
+#Март
+for i in range (1, 32):
+     if (i>9):
+            d1 = i % 10
+             
+            d2 = i % 100 // 10
+            result+= d1 + d2
+     if(i<10):
+             result+=i
+#Апрель
+for i in range (1, 31):
+    if (i>9):
+            d1 = i % 10
+             
+            d2 = i % 100 // 10
+            result+= d1 + d2
+    if(i<10):
+             result+=i
+#Май
+for i in range (1, 32):
+    if (i>9):
+            d1 = i % 10
+             
+            d2 = i % 100 // 10
+            result+= d1 + d2
+    if(i<10):
+             result+=i
+#Июнь
+for i in range (1, 31):
+    if (i>9):
+            d1 = i % 10
+             
+            d2 = i % 100 // 10
+            result+= d1 + d2
+    if(i<10):
+             result+=i
+#Июль
+for i in range (1, 32):
+    if (i>9):
+            d1 = i % 10
+             
+            d2 = i % 100 // 10
+            result+= d1 + d2
+    if(i<10):
+             result+=i
+#Август
+
+for i in range (1, 32):
+    if (i>9):
+            d1 = i % 10
+             
+            d2 = i % 100 // 10
+            result+= d1 + d2
+    if(i<10):
+             result+=i
+#Сентябрь
+for i in range (1, 31):
+    if (i>9):
+            d1 = i % 10
+             
+            d2 = i % 100 // 10
+            result+= d1 + d2
+    if(i<10):
+             result+=i
+#Октябрь
+for i in range (1, 32):
+    if (i>9):
+            d1 = i % 10
+             
+            d2 = i % 100 // 10
+            result+= d1 + d2
+    if(i<10):
+             result+=i
+#Ноябрь
+for i in range (1, 31):
+    if (i>9):
+            d1 = i % 10
+             
+            d2 = i % 100 // 10
+            result+= d1 + d2
+    if(i<10):
+             result+=i
+#Декабрь
+for i in range (1, 32):
+    if (i>9):
+            d1 = i % 10
+             
+            d2 = i % 100 // 10
+            result+= d1 + d2
+    if(i<10):
+            result+=i
+print('Результат:', result)
  
 
  
